5f55622053bd18523305c6866e6b94030cc68356
Yeon <choi21660774@gmail.com>
2025-04-30 14:07:05 +0900

    first commit

---
+++ Backend/src/main/java/curtaincall/CurtaincallApplication.java
@@ -0,0 +1,13 @@
+package curtaincall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtaincallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurtaincallApplication.class, args);
+	}
+
+}
