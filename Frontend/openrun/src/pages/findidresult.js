b973477ff26aecb45a21474db541bd9de296f2c7
Dain Hur <C258028@g.hongik.ac.kr>
2025-05-05 23:06:50 +0900

    feat/mypage

--- Frontend/openrun/src/pages/findidresult.js
+++ Frontend/openrun/src/pages/findidresult.js
@@ -6,17 +6,21 @@ import logo from "../components/logo.png";
 const Findidresult = () => {
   return (
     <div>
-        <form className="findidresultform">
-      <NavLink to="/">
-        <img src={logo} alt="로고" className="findidresult_logo" />
-      </NavLink>
-      <div className="findid_result">
-        <p>
-          귀하의 아이디는{" "}
-          <span style={{ color: "#9F0000" }}>exampleUserId123</span>입니다.{/* 유저 아이디 DB에서 전달받아 출력 */}
-        </p>
-      </div>
-      <NavLink to="/" id="sbtn"> 로그인하러 가기 </NavLink>
+      <form className="findidresultform">
+        <NavLink to="/">
+          <img src={logo} alt="로고" className="findidresult_logo" />
+        </NavLink>
+        <div className="findid_result">
+          <p>
+            귀하의 아이디는{" "}
+            <span style={{ color: "#9F0000" }}>exampleUserId123</span>입니다.
+            {/* 유저 아이디 DB에서 전달받아 출력 */}
+          </p>
+        </div>
+        <NavLink to="/login" id="sbtn">
+          {" "}
+          로그인하러 가기{" "}
+        </NavLink>
       </form>
     </div>
   );
