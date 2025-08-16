457c89faede8c3c80f8908cb2bf12e6b86476e55
SsssYeon <choi21660774@gmail.com>
2025-08-17 04:46:45 +0900

    WebConfig.java modify

--- Backend/src/main/java/com/openrun/config/WebConfig.java
+++ Backend/src/main/java/com/openrun/config/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000")  // 허용할 출처
+                .allowedOrigins(
+                        "https://openrun.netlify.app",
+                        "http://localhost:3000" // 로컬 테스트
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 허용할 헤더
                 .allowCredentials(true);  // 쿠키 인증 허용 여부
